771a9e4c776c1a5c87274550613bf7c8c5061382
naari3 <naari.named@gmail.com>
2023-07-27 08:01:19 +0900

    add test

--- src/mogi_result.rs
+++ src/mogi_result.rs
@@ -25,14 +25,19 @@ impl MogiResult {
 
     pub fn set_current_position(&mut self, position: Position) {
         let current_course = self.current_course.clone();
-        let race = RaceResult::new(current_course, position);
-        println!("race: {:?}", race);
-        self.races.push(race);
-        self.current_course = None;
+        if let Some(current_course) = current_course {
+            let race = RaceResult::new(Some(current_course), position);
+            self.races.push(race);
+            self.current_course = None;
+        }
     }
 
     pub fn reset_current_course(&mut self) {
         self.current_course = None;
+    }
+
+    pub fn total_score(&self) -> u32 {
+        self.races.iter().map(|r| r.to_score()).sum::<u32>()
     }
 }
 
@@ -46,8 +51,35 @@ impl Display for MogiResult {
         if let Some(current_course) = current_course {
             writeln!(f, "current course: {}", current_course)?;
         }
-        let total_score = &self.races.iter().map(|r| r.to_score()).sum::<u32>();
+        let total_score = self.total_score();
         writeln!(f, "total score: {total_score}")?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::courses::Series;
+
+    use super::*;
+
+    #[test]
+    fn test_mogi_result() {
+        let mut mogi_result = MogiResult::new();
+        mogi_result.set_current_course(Course::new("ドルフィンみさき".to_string(), Series::New));
+        mogi_result.set_current_position(Position::First);
+        assert_eq!(mogi_result.races.len(), 1);
+
+        mogi_result.set_current_course(Course::new("ヨッシーアイランド".to_string(), Series::New));
+        mogi_result.set_current_position(Position::Second);
+        assert_eq!(mogi_result.total_score(), 27);
+    }
+
+    #[test]
+    fn test_mogi_result_reset_current_course() {
+        let mut mogi_result = MogiResult::new();
+        mogi_result.set_current_course(Course::new("ドルフィンみさき".to_string(), Series::New));
+        mogi_result.reset_current_course();
+        assert_eq!(mogi_result.current_course, None);
     }
 }
